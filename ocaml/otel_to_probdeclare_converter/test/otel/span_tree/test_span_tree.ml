c084f621ecd3d1121d4c562b78bdfc4f61ca7951
freiseisen <lukas.freiseisen@scch.at>
2024-09-02 17:17:52 +0200

    intermediate commit - WiP

--- ocaml/otel_to_probdeclare_converter/test/otel/span_tree/test_span_tree.ml
+++ ocaml/otel_to_probdeclare_converter/test/otel/span_tree/test_span_tree.ml
@@ -11,11 +11,11 @@ let test_generate_nodes _ =
   let spans = json |> Yojson.Basic.Util.to_list in
   let decoded = List.map decode_trace_span spans in
   let roots, nodes = generate_nodes decoded in
-  List.iter (pp_span_tree Format.std_formatter) span_trees; // FIXME 
+  List.iter (pp_span_tree Format.std_formatter) roots;
+  List.iter (pp_span_tree Format.std_formatter) nodes;
   assert_bool "" true
 
-
-let test_generate_span_trees_from_spans _ =
+(*let test_generate_span_trees_from_spans _ =
   let json =
     Yojson.Basic.from_file
       (Sys.getcwd ()
@@ -25,13 +25,14 @@ let test_generate_span_trees_from_spans _ =
   let decoded = List.map decode_trace_span spans in
   let span_trees = generate_span_trees_from_spans decoded in
   List.iter (pp_span_tree Format.std_formatter) span_trees;
-  assert_bool "" true
+  assert_bool "" true*)
 
 let suite =
   "span_tree_test_suite"
   >::: [
-         "test_generate_span_trees_from_spans"
-         >:: test_generate_span_trees_from_spans;
+         (*"test_generate_span_trees_from_spans"
+           >:: test_generate_span_trees_from_spans;*)
+         "test_generate_nodes" >:: test_generate_nodes;
        ]
 
 let () = run_test_tt_main suite
